41a728488cd27836229198faac1cfae2baf676ea
Nischal Subedi <nischal1337@gmail.com>
2024-06-12 18:43:59 +0545

    input validation

---
+++ week3/auth.js
@@ -0,0 +1,30 @@
+// authentication and input validation without using middlewares
+
+const express = require("express");
+const app = express();
+app.get("/health-checkup", function (req, res) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.userId;
+
+    if (username != "admin" || password != "admin") {
+        res.status(400).json({
+            "msg": "Something is wrong with your input"
+        })
+        return
+    }
+
+
+    if (kidneyId = 1 && kidneyId != 2) {
+        res.status(400).json({
+            "msg": "there is something wrong with your inputs"
+        })
+        return
+    }
+
+    res.json({
+        msg: "Your kidneys are healthy"
+    })
+
+});
+app.listen(3000);
