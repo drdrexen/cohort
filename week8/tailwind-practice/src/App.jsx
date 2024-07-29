7eb47f00b95166c72a7268b9f08235f2d33b14ff
Nischal Subedi <nischal1337@gmail.com>
2024-07-29 10:59:53 +0545

    update: button

--- week8/tailwind-practice/src/App.jsx
+++ week8/tailwind-practice/src/App.jsx
@@ -3,30 +3,12 @@
 function App() {
 
 
-  return (<>
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
-      <div style={{ backgroundColor: "red" }}>hi</div>
-      <div style={{ backgroundColor: "blue" }}>hi</div>
-      <div style={{ backgroundColor: "green" }}>hi</div>
-    </div>
+  return (<div className="grid grid-cols-1 md:grid-cols-3">
+    <div className=" bg-green-500 "> hi there</div>
+    <div className=" bg-yellow-500 "> hi there</div>
+    <div className=" bg-red-600 "> hi there</div>
 
-    // using tailwind
-    <div className="flex justify-between ">
-      <div className="bg-pink-800">Nischal</div>
-      <div className="bg-red-500">Nischal2</div>
-      <div className="bg-green-500">Nischal3</div>
-
-    </div>
-
-    <br /><br /><br />
-    {/* grid  */}
-    <div className="grid grid-cols-5" >
-      <div className="bg-pink-800 col-span-1">Div 1</div>
-      <div className="bg-red-500 col-span-1">Div 2</div>
-      <div className="bg-green-500 col-span-3">Div 3</div>
-
-    </div>
-  </>
+  </div >
   )
 }
 
